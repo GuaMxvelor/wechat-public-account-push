ca00a8b523a05d3e862f62ff95e35b5f53531257
GuaMxvelor <116094567+GuaMxvelor@users.noreply.github.com>
2022-10-23 02:58:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -47,7 +47,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o_vsE6d17AyxeJ825IYnvQtGnAWU',
+      id: 'o_vsE6a_XyOgXUBVbkyAJQht6LcQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'He7sRd8wmzZV5ZFbqXzRmprtF--N23iA0wyaKcvOFUg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
